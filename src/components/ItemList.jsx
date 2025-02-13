5f3825ba774dca4ce4c08e59d488b170a3fbbe4b
Bernardo Diniz <bernardoldiniz04@gmail.com>
2025-02-12 21:55:36 -0300

    refatorando ItemList

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,50 @@
+import SingleItem from "./SingleItem";
+
+function ItemList({ title, items }) {
+
+    return (
+        <div>
+
+            <div className="item-list">
+                <div className="item-list__header">
+                    <h2>{title} populares</h2>
+                    <a className="item-list__link" href="/">
+                        Mostrar tudo
+                    </a>
+                </div>
+
+                <div className="item-list__container">
+
+                    {
+                        items === 5 ? (
+                        <>
+                            <SingleItem/>
+                            <SingleItem/>
+                            <SingleItem/>
+                            <SingleItem/>
+                            <SingleItem/>
+                        </> ) : (
+                            <>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                                <SingleItem/>
+                            </>
+                        )
+                    }
+
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ItemList
